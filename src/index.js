8188c39b8ce440d45a42b6543a45f0574a971203
FreeFallRush <queenofcups.altbot@gmail.com>
2023-06-06 18:49:58 +0300

    updated index.js with openProjectModal on btn click

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import "./style.css";
 import handlers from "./js/handlers";
 
+const addNewProjectBtn = document.querySelector("[data-add-new-project]");
+
+addNewProjectBtn.addEventListener("click", () => {
+  handlers.openNewProjectModal();
+});
+
 handlers.toggleSidebar();
