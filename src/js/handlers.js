46caf30874ce94368b838d2dfa97b8030a80db72
FreeFallRush <queenofcups.altbot@gmail.com>
2023-06-06 18:53:36 +0300

    updated handlers.js with openNewProjectModal and close modal

--- src/js/handlers.js
+++ src/js/handlers.js
@@ -1,3 +1,4 @@
+import domElements from "./dom-elements";
 const handlers = (() => {
   const toggleSidebar = () => {
     const nav = document.querySelector("nav");
@@ -10,8 +11,37 @@ const handlers = (() => {
     };
   };
 
+  //Project Modals
+
+  const modal = document.querySelector("[data-modal]");
+  const closeModalBtn = document.querySelector("[data-close-modal]");
+  const modalForm = document.querySelector("[data-form]");
+
+  closeModalBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    modal.classList.add("hidden");
+  });
+
+  window.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      e.preventDefault();
+
+      modal.classList.add("hidden");
+    }
+  });
+
+  //Add Project
+
+  const openNewProjectModal = () => {
+    domElements.createAddProjectModal();
+
+    modal.classList.remove("hidden");
+  };
+
   return {
     toggleSidebar,
+    openNewProjectModal,
   };
 })();
 export default handlers;
