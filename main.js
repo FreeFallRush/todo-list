d6055238b676bfa7bae4df29e9641b65c1e67dfb
FreeFallRush <queenofcups.altbot@gmail.com>
2023-06-06 18:53:36 +0300

    updated handlers.js with openNewProjectModal and close modal

--- main.js
+++ main.js
@@ -789,6 +789,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
+/* harmony import */ var _dom_elements__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(21);
+
 const handlers = (() => {
   const toggleSidebar = () => {
     const nav = document.querySelector("nav");
@@ -801,11 +803,182 @@ const handlers = (() => {
     };
   };
 
+  //Project Modals
+
+  const modal = document.querySelector("[data-modal]");
+  const closeModalBtn = document.querySelector("[data-close-modal]");
+  const modalForm = document.querySelector("[data-form]");
+
+  closeModalBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    modal.classList.add("hidden");
+  });
+
+  window.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      e.preventDefault();
+
+      modal.classList.add("hidden");
+    }
+  });
+
+  //Add Project
+
+  const openNewProjectModal = () => {
+    _dom_elements__WEBPACK_IMPORTED_MODULE_0__["default"].createAddProjectModal();
+
+    modal.classList.remove("hidden");
+  };
+
   return {
     toggleSidebar,
+    openNewProjectModal,
   };
 })();
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (handlers);
+
+
+/***/ }),
+/* 21 */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+const domElements = (() => {
+  //Main dom elements
+  const createH2 = (className) => {
+    const h2 = document.createElement("h2");
+    h2.classList.add(className);
+    return h2;
+  };
+
+  const createH3 = (className) => {
+    const h3 = document.createElement("h3");
+    h3.classList.add(className);
+    return h3;
+  };
+
+  const createPara = (className) => {
+    const p = document.createElement("p");
+    p.classList.add(className);
+    return p;
+  };
+
+  const createDiv = (className) => {
+    const div = document.createElement("div");
+    div.classList.add(className);
+    return div;
+  };
+
+  const createButton = (className) => {
+    const button = document.createElement("button");
+    button.classList.add(className);
+    return button;
+  };
+
+  const createLabel = (className) => {
+    const label = document.createElement("label");
+    label.classList.add(`${className}-label`);
+    label.setAttribute("for", `${className}-input`);
+    return label;
+  };
+
+  const createInput = (className, inputType) => {
+    const input = document.createElement("input");
+    input.classList.add(`${className}-input`);
+    input.setAttribute("type", inputType);
+    input.setAttribute("id", `${className}-input`);
+    return input;
+  };
+
+  const createTextArea = (className) => {
+    const textArea = document.createElement("textarea");
+    textArea.classList.add(`${className}-input`);
+    textArea.setAttribute("id", `${className}-input`);
+    textArea.setAttribute("name", `${className}-input`);
+    return textArea;
+  };
+
+  const createSelectElement = (className) => {
+    const selectElement = document.createElement("select");
+    selectElement.setAttribute("name", `${className}-select`);
+    selectElement.setAttribute("id", `${className}-select`);
+    return selectElement;
+  };
+
+  const createOption = (value) => {
+    const option = document.createElement("option");
+    option.setAttribute("value", value);
+    return option;
+  };
+
+  //Modals
+  const createAddProjectModal = () => {
+    const divTitle = createDiv("modal-title");
+    const modalTitle = createH2("modal-new-project-title");
+    modalTitle.textContent = "Add New Project";
+
+    const divProjectTitleInput = createDiv("project-name-form");
+    const inputNameText = createH3("name-input-text");
+    inputNameText.textContent = "Project Name:";
+    const newProjectLabel = createLabel("new-project");
+    const newProjectInput = createInput("new-project", "text");
+    newProjectInput.placeholder = "How would you call it?";
+    newProjectInput.setAttribute("data-new-project-name", "");
+    newProjectInput.maxLength = 50;
+
+    const divProjectDescriptionInput = createDiv("project-description-form");
+    const inputDescriptionText = createH3("description-input-text");
+    inputDescriptionText.textContent = "Project Description:";
+    const newProjectDescriptionLabel = createLabel("new-project-description");
+    const newProjectDescriptionInput = createTextArea(
+      "new-project-description"
+    );
+    newProjectDescriptionInput.placeholder =
+      "Your choice if you want to add a short description to your project...";
+    newProjectDescriptionInput.setAttribute("data-new-project-description", "");
+    newProjectDescriptionInput.maxLength = 350;
+
+    const divColorInput = createDiv("project-color-form");
+    const inputColorText = createH3("color-input-text");
+    inputColorText.textContent = `Project's Color:`;
+    const colorInputLabel = createLabel("project-color");
+    const colorInput = createInput("project-color", "color");
+    colorInput.setAttribute("data-project-color", "");
+
+    formContainer.textContent = "";
+
+    divTitle.appendChild(modalTitle);
+    divProjectTitleInput.append(
+      inputNameText,
+      newProjectLabel,
+      newProjectInput
+    );
+    divProjectDescriptionInput.append(
+      inputDescriptionText,
+      newProjectDescriptionLabel,
+      newProjectDescriptionInput
+    );
+
+    divColorInput.append(inputColorText, colorInputLabel, colorInput);
+
+    formContainer.append(
+      divTitle,
+      divProjectTitleInput,
+      divProjectDescriptionInput,
+      divColorInput
+    );
+  };
+
+  return {
+    createAddProjectModal,
+  };
+})();
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (domElements);
 
 
 /***/ })
